73713c765dfd48b383e9e5b86008bef9042fccce
Corey Hastings <stingzlightingdesign@hotmail.com>
2022-09-24 00:46:17 +0000

    Implemented S3 interactions

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open('README.md', 'r') as f:
 setup(
     name='pgpackup',
     version='0.1.0',
-    author='Keith Thompson',
-    author_email='keith@linuxacademy.com',
+    author='Corey Hastings',
+    author_email='stingzlightingdesign@hotmail.com',
     description='A utility for backing up PostgreSQL databases',
     long_description=long_description,
     long_description_content_type='text/markdown',
